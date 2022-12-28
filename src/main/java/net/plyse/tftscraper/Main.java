01f5f8632acf29489aa2c6930ddd78b829cd2acf
Raphael Dichler <raphael@dichler.com>
2022-12-28 23:27:07 +0100

    added jsoup for scraping

--- src/main/java/net/plyse/tftscraper/Main.java
+++ src/main/java/net/plyse/tftscraper/Main.java
@@ -1,13 +1,29 @@
 package net.plyse.tftscraper;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 /**
  * @author Raphael Dichler on 28.12.2022.
  */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String url = "https://tftactics.gg/champions/jax";
 
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(doc);
+
+        String title = doc.title();
+        System.out.println(title);
     }
 
 }
