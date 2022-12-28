949d15453a53d3ba6d15b0d734994c6a42add7a6
Raphael Dichler <raphael@dichler.com>
2022-12-28 23:30:15 +0100

    added jsoup for scraping

--- src/main/java/net/plyse/tftscraper/ability/AbilityCost.java
+++ src/main/java/net/plyse/tftscraper/ability/AbilityCost.java
@@ -4,4 +4,7 @@ package net.plyse.tftscraper.ability;
  * @author Raphael Dichler on 28.12.2022.
  */
 public interface AbilityCost {
+
+    boolean isAbilityUp();
+
 }
--- src/main/java/net/plyse/tftscraper/ability/AbilityLevelBonus.java
+++ src/main/java/net/plyse/tftscraper/ability/AbilityLevelBonus.java
@@ -1,7 +1,19 @@
 package net.plyse.tftscraper.ability;
 
+import lombok.AllArgsConstructor;
+import net.plyse.tftscraper.champion.Champion;
+
 /**
  * @author Raphael Dichler on 28.12.2022.
  */
+@AllArgsConstructor
 public abstract class AbilityLevelBonus {
+
+    protected Champion champion;
+
+    /**
+     *  Updates the champions states based on the current level of the champion
+     */
+    public abstract void update();
+
 }
